b1666253b19d275244a0d470a3ff24921887852a
travis souther <souther-trav@hotmail.com>
2024-02-24 22:00:34 -0600

    css added

--- src/components/PollComponent.jsx
+++ src/components/PollComponent.jsx
@@ -13,8 +13,8 @@ export default function PollComponent() {
 <main className="grid gap-4 md:gap-6 p-4 md:p-6">
 <Card>
   <CardHeader id="form-Id">
-    <CardTitle className="text-yellow-600">What is your favorite color?</CardTitle>
-    <CardDescription className="text-sm">Choose the color that brings you the most joy.</CardDescription>
+    <CardTitle>What is your favorite color?</CardTitle>
+    <CardDescription id="form-Id2">Choose the color that brings you the most joy.</CardDescription>
   </CardHeader>
   <CardContent>
 
