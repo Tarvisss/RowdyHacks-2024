3921356dedf6e488eed1d1d0d78816d733c5f72a
Squire <diane.squire@swri.org>
2024-02-25 09:09:37 -0600

    added handlers for login and submit buttons

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,3 +1,5 @@
+
+'use client'
 import {
     Dialog,
     DialogContent,
@@ -11,81 +13,114 @@ import {
 import { Button } from "@/components/ui/button"
 import { Label } from "@/components/ui/label"
 import { Input } from "@/components/ui/input"
+import { useState } from "react"
 export default function Login() {
-    return (
-   
+  // REACT STATES
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-        <Dialog>
+  const onLogin = (event) => {
+    console.log(phoneNumber, password);
+    //TODO: push info to backend
+  };
+  const onCreateNewAccount = (event) => {
+    console.log(username, phoneNumber, password);
+    //TODO: push info to backend
+  };
+
+
+  return (
+    <Dialog>
       <DialogTrigger asChild>
         <Button variant="outline" className = "bg-green-600 text-white">Login</Button>
       </DialogTrigger>
       <DialogContent className="sm:max-w-[425px]">
         <DialogHeader>
           <DialogTitle>Login</DialogTitle>
-         
         </DialogHeader>
         <div className="grid gap-4 py-4">
           <div className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="name" className="text-right"type="text">
               Phone Number
             </Label>
-            <Input id="name"  className="col-span-3" />
+            <Input 
+              id="name" 
+              value={phoneNumber} 
+              onChange={e => setPhoneNumber(e.target.value)} 
+              className="col-span-3" 
+              type="text" />
           </div>
           <div className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="username" className="text-right"type="text">
               Password
             </Label>
-            <Input id="username"  className="col-span-3" type="text"/>
+            <Input 
+              id="password" 
+              value={password} 
+              onChange={e => setPassword(e.target.value)} 
+              className="col-span-3" 
+              type="text"/>
           </div>
-        </div>
+        </div>       
         <div className="grid grid-cols-2 gap-4 py-4">
         <DialogClose asChild>
-          <Button type="submit">Login</Button>
+          <Button type="submit" onClick={onLogin}>Login</Button>
         </DialogClose>
+        
+        
         <Dialog>
-      <DialogTrigger asChild>
-        <Button variant="outline" className = "bg-green-600 text-white">Sign Up</Button>
-      </DialogTrigger>
-      
-      <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>Create New Account</DialogTitle>
-         
-        </DialogHeader>
-        <div className="grid gap-4 py-4">
-        
-          <div className="grid grid-cols-4 items-center gap-4">
-            
-            <Label htmlFor="name" className="text-right">
-              Name
-            </Label>
-            <Input id="name"  className="col-span-3" />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="username" className="text-right">
-              Phone Number
-            </Label>
-            <Input id="username"  className="col-span-3" type="text" />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="username" className="text-right" type="text">
-              Password
-            </Label>
-            <Input id="password"  className="col-span-3" />
-            
-          </div>
-        
+          <DialogTrigger asChild>
+            <Button variant="outline" className = "bg-green-600 text-white">Sign Up</Button>
+          </DialogTrigger>      
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>Create New Account</DialogTitle>            
+            </DialogHeader>
+            <div className="grid gap-4 py-4">            
+              <div className="grid grid-cols-4 items-center gap-4">                
+                <Label htmlFor="name" className="text-right">
+                  Name
+                </Label>
+                <Input 
+                  id="name" 
+                  value={username} 
+                  onChange={e => setUsername(e.target.value)}
+                  className="col-span-3"
+                  type="text" />
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="username" className="text-right">
+                  Phone Number
+                </Label>
+                <Input 
+                  id="username" 
+                  value={phoneNumber} 
+                  onChange={e => setPhoneNumber(e.target.value)} 
+                  className="col-span-3"
+                  type="text" />
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="username" className="text-right">
+                  Password
+                </Label>
+                <Input 
+                  id="password" 
+                  value={password} 
+                  onChange={e => setPassword(e.target.value)} 
+                  className="col-span-3"
+                  type="text" />
+              </div>            
+            </div>
+            <DialogClose asChild>
+              <Button type="submit" onClick={onCreateNewAccount}>Submit</Button>
+            </DialogClose>
+          </DialogContent>
+        </Dialog>
+
+
         </div>
-
-        <DialogClose asChild>
-          <Button type="submit">Submit</Button>
-        </DialogClose>
-
       </DialogContent>
     </Dialog>
-        </div>
-      </DialogContent>
-    </Dialog>
-
-    )
+  )
 }
