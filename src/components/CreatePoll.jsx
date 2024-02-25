4be651b31fc2a16a78a823dc9d30d09fbbc44a79
travis souther <souther-trav@hotmail.com>
2024-02-25 11:15:48 -0600

    added submit

--- src/components/CreatePoll.jsx
+++ src/components/CreatePoll.jsx
@@ -13,11 +13,36 @@ import {
 } from "@/components/ui/dialog"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import axios from "axios"
 import { useState } from "react"
  
 export default function CreatePoll() {
 
+const [title, setTitle] = useState("");
+const [description, setDescription] = useState("");
 const [pollTime, setPollTime] = useState("");
+const [setnotes, setNotes] = useState("");
+
+const submitPoll = () =>{
+const submitinfo = {
+  title: title,
+  description: description,
+  expires_at: pollTime,
+  notes: setnotes,
+}
+  axios
+  .post("https://rowdyhacks-30994aeafc3f.herokuapp.com/polls", submitinfo )
+  .then(res => {
+    console.log(res);
+    if(res.status == 200){
+      console.log('poll created', pollTime);
+    }
+  })
+  .catch(err => {
+   console.log(err.message);
+  });
+console.log(event.target.value);
+}
 
   return (
     <Dialog>
@@ -34,6 +59,32 @@ const [pollTime, setPollTime] = useState("");
         <div className="grid gap-4 py-4">
           <div className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="time" className="text-right">
+              Title:
+            </Label>
+          <Input 
+            id="title" 
+            value={title} 
+            className="col-span-3" 
+            onChange={(e) => {setTitle(e.target.value)}}
+            type="text"
+            />
+
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="time" className="text-right">
+              Discription:
+            </Label>
+          <Input 
+            id="description" 
+            value={description} 
+            className="col-span-3" 
+            onChange={(e) => {setDescription(e.target.value)}}
+            type="text"
+            />
+
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="time" className="text-right">
               Time of Event:
             </Label>
           <Input 
@@ -45,9 +96,22 @@ const [pollTime, setPollTime] = useState("");
             />
 
           </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="time" className="text-right">
+              Notes:
+            </Label>
+          <Input 
+            id="notes" 
+            value={setnotes} 
+            className="col-span-3" 
+            onChange={(e) => {setNotes(e.target.value)}}
+            type="text"
+            />
+
+          </div>
         </div>
         <DialogClose asChild>
-          <Button type="submit">Submit</Button>
+          <Button type="submit" onClick={submitPoll}>Submit</Button>
         </DialogClose>
       </DialogContent>
     </Dialog>
