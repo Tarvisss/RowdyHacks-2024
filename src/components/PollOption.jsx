7532dcfb839b428256b564ad798e74f75f8ab7b7
phamkevinusa <phamkevinusa@gmail.com>
2024-02-24 21:36:26 -0600

    added padding to poll option

--- src/components/PollOption.jsx
+++ src/components/PollOption.jsx
@@ -5,7 +5,7 @@ import { Input } from "@/components/ui/input"
 export default function PollOption(props) {
     return(
 <li>
-        <Label className="flex items-center">
+        <Label className="flex items-center pt-4">
           <Input className="w-7 h-8 rounded-full" type="checkbox" />
           <span className="ml-2.5 text-sm sm:text-base">{props.option}</span>
         </Label>
