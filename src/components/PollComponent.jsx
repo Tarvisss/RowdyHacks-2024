409ff2484673b2d3466d5a840ab87fc32227125d
Squire <diane.squire@swri.org>
2024-02-24 21:18:11 -0600

    template empty function for updating backend when poll is changed

--- src/components/PollComponent.jsx
+++ src/components/PollComponent.jsx
@@ -1,41 +1,46 @@
+'use client'
+
 import { CardTitle, CardDescription, CardHeader, CardContent, Card } from "@/components/ui/card"
 // import { Label } from "@/components/ui/label"
 // import { Input } from "@/components/ui/input"
 import PollOption from "./PollOption"
 
 export default function PollComponent() {
+
+  
+
   return(
-<main className="grid gap-4 md:gap-6 p-4 md:p-6">
-<Card>
-  <CardHeader className="bg-yellow-100 dark:bg-yellow-300">
-    <CardTitle className="text-yellow-600">What is your favorite color?</CardTitle>
-    <CardDescription className="text-sm">Choose the color that brings you the most joy.</CardDescription>
-  </CardHeader>
-  <CardContent>
+    <main className="grid gap-4 md:gap-6 p-4 md:p-6">
+    <Card>
+      <CardHeader className="bg-yellow-100 dark:bg-yellow-300">
+        <CardTitle className="text-yellow-600">What is your favorite color?</CardTitle>
+        <CardDescription className="text-sm">Choose the color that brings you the most joy.</CardDescription>
+      </CardHeader>
+      <CardContent>
 
 
-    <form>
+        <form>
 
-      <PollOption option="yes" />
-      <PollOption option="maybe" />
-      <PollOption option="no" />
+          <PollOption option="yes" />
+          <PollOption option="maybe" />
+          <PollOption option="no" />
 
-      <div className="form-group">
-        <button className="btn btn-primary mt-2" type="submit">
-          Save
-        </button>
-      </div>
+          <div className="form-group">
+            <button className="btn btn-primary mt-2" type="submit">
+              Save
+            </button>
+          </div>
 
-    </form>
-    
-    <ul className="grid gap-2.5 sm:gap-3.5">
-      
-      
-      
-      
-    </ul>
-  </CardContent>
-</Card>
-</main>
+        </form>
+        
+        <ul className="grid gap-2.5 sm:gap-3.5">
+          
+          
+          
+          
+        </ul>
+      </CardContent>
+    </Card>
+    </main>
   )
 }
--- src/components/PollOption.jsx
+++ src/components/PollOption.jsx
@@ -3,17 +3,24 @@ import { Label } from "@/components/ui/label"
 import { Input } from "@/components/ui/input"
 
 export default function PollOption(props) {
-    return(
-      <div className="form-check">
-        <label>
-          <input
-            type="radio"
-            name="react-tips"
-            value={props.option}
-            className="form-check-input"
-          />
-          {props.option}
-        </label>
-      </div>
+
+  function handleChange(event) {
+    //TODO: add call to backend to set the vote to props.options
+    console.log(event.target.value);
+  }
+
+  return(
+    <div className="form-check">
+      <label>
+        <input
+          type="radio"
+          name="react-tips"
+          value={props.option}
+          onChange={handleChange}
+          className="form-check-input"
+        />
+        {props.option}
+      </label>
+    </div>
   )
 }
